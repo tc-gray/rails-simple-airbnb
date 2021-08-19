0055711cd7c8bcf85199b98ad4e294f34be478cb
Tegan Cadney-Gray <tegan.cadney@gmail.com>
2021-08-19 12:14:53 +0100

    initial layout for simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'clear db'
+Flat.destroy_all
+puts 'db cleared'
+
+4.times do
+  Flat.create(
+    name: Faker::Artist.name,
+    address: Faker::Address.street_address,
+    description: Faker::Hipster.sentence,
+    price_per_night: rand(10..500),
+    number_of_guests: rand(1..6)
+  )
+  puts '1 added....'
+end
+
+puts 'complete'
